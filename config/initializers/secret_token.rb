0ddd73f6e42e95487bbbddb33af21c95c0dc5d1b
madflow3 <alexandre@gmail.com>
2014-04-02 02:55:14 +0200

    good

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Changetaville::Application.config.secret_key_base = '76112ee5209569514c4cbf421b79cee6b803dc235bd3428e15650010b856884636060b021a6229fd1d6078f0e1c9abcbe79b67f2cf7ec03cb3014caf68a625c8'
